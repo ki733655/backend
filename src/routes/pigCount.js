d2f91d145e37ba56a5e738e28395d3507e1b9511
ki733655 <143171590+ki733655@users.noreply.github.com>
2024-03-26 21:34:34 +0530

    route for realtime

---
+++ src/routes/pigCount.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const {Boar,Sow, Piglets, Khassi} = require("../models/pigmodel")
+
+const router = express.Router();
+
+// boar count route -----------------------------------------------------
+router.get("/boar-count", async(req, res) => {
+    try {
+        const count = await Boar.countDocuments();
+        res.json({ count });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+ 
+})
+// sow count route----------------------------------------------------------------
+router.get("/sow-count", async(req, res) => {
+    try {
+        const count = await Sow.countDocuments();
+        res.json({ count });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+ 
+})
+// piglets count route----------------------------------------------------------------
+router.get("/piglets-count", async(req, res) => {
+    try {
+        const count = await Piglets.countDocuments();
+        res.json({ count });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+ 
+})
+// khassi count route
+router.get("/khassi-count", async(req, res) => {
+    try {
+        const count = await Khassi.countDocuments();
+        res.json({ count });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+ 
+})
+
+module.exports = router;
