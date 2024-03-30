e7a939ce2644c06a8a37e4876547ee01bb92df9f
ki733655 <143171590+ki733655@users.noreply.github.com>
2024-03-30 22:09:50 +0530

    Search feature

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,13 +5,15 @@ const pigsRoutes = require("./pigRoutes")
 const pigCount = require("./pigCount")
 const pigDetails = require("./pigDetails")
 const pigDelete = require("./pigDelete")
+const pigEdit = require("./pigEdit")
+const pigSearch = require("./pigSearch")
 
 router.get("/", (req, res) => {
     res.send("Hello World!");
   });
 
   // Pig entry route 
-  router.use(pigsRoutes,pigCount,pigDetails, pigDelete)
+  router.use(pigsRoutes,pigCount,pigDetails, pigDelete, pigEdit,pigSearch)
 
 
 
